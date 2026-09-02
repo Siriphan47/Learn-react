592f13198b796076c02c2a4c721aeab4eb081902
Siriphan47 <siriphansithisack@gmail.com>
2026-09-02 18:57:27 +0700

    Learn About Array State

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
-function App() {
+import { useState } from "react";
 
-  return <h1>Hello World</h1>;
+function App() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "Bill",
+      Gender: "Male",
+    },
+    {
+      id: 2,
+      name: "Nam",
+      Gender: "Female",
+    },
+    {
+      id: 3,
+      name: "Alex",
+      Gender: "Female",
+    },
+  ]);
+
+  return (
+    <>
+      <h1>Population {data.length} Peoples</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            {item.name} | {item.Gender}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
