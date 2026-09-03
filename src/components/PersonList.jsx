2f9308f1776897016a8d20dc992099abe44562db
Siriphan47 <siriphansithisack@gmail.com>
2026-09-03 19:06:11 +0700

    Learn how to add and use image in React

--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import boy from "../assets/boy.svg";
+import girl from "../assets/girl.svg";
 function PersonList() {
   const [data, setData] = useState([
     {
@@ -17,18 +18,29 @@ function PersonList() {
       name: "Alex",
       Gender: "Female",
     },
+    {
+      id: 3,
+      name: "Jame",
+      Gender: "Male",
+    },
   ]);
   const [show, setShow] = useState(true);
 
   return (
     <>
       <h1>Population {data.length} Peoples</h1>
+      <img src={girl} width={50} height={50} />
       <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
       <ul>
         {show &&
           data.map((item) => (
             <li key={item.id}>
-              {item.name} | {item.Gender}
+              <img
+                src={item.Gender === "Male" ? boy : girl}
+                width={50}
+                height={50}
+              />
+              <h3>{item.name}</h3>
             </li>
           ))}
       </ul>
