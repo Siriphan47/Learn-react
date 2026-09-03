cb97bdfa4fafafca03785960b8dde7d63689ff01
Siriphan47 <siriphansithisack@gmail.com>
2026-09-03 18:51:40 +0700

    Learn how to create component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
 import { useState } from "react";
+import Header from "./components/Header";
+import PersonList from "./components/PersonList";
 
 function App() {
-  const [data, setData] = useState([
-    {
-      id: 1,
-      name: "Bill",
-      Gender: "Male",
-    },
-    {
-      id: 2,
-      name: "Nam",
-      Gender: "Female",
-    },
-    {
-      id: 3,
-      name: "Alex",
-      Gender: "Female",
-    },
-  ]);
-
-  const [show, setShow] = useState(true);
-
   return (
     <>
-      <h1>Population {data.length} Peoples</h1>
-      <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
-      <ul>
-        {show &&
-          data.map((item) => (
-            <li key={item.id}>
-              {item.name} | {item.Gender}
-            </li>
-          ))}
-      </ul>
+      <Header />
+      <PersonList />
     </>
   );
 }
---
+++ src/components/PersonList.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function PersonList() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "Bill",
+      Gender: "Male",
+    },
+    {
+      id: 2,
+      name: "Nam",
+      Gender: "Female",
+    },
+    {
+      id: 3,
+      name: "Alex",
+      Gender: "Female",
+    },
+  ]);
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      <h1>Population {data.length} Peoples</h1>
+      <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+      <ul>
+        {show &&
+          data.map((item) => (
+            <li key={item.id}>
+              {item.name} | {item.Gender}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
+
+export default PersonList;
